3b0a61c233af5b0fe23f46f29260b739aa2cd01d
yizhi.li <tgxpuisb@hotmail.com>
2017-05-18 23:32:05 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const PSD = require('psd')
+const fs = require('fs')
+
+var psd = PSD.fromFile('./test.psd')
+psd.parse()
+
+let obj = psd.tree().export()
+
+let target = {}
+
+target.width = obj.document.width
+target.height = obj.document.height
+target.background = ''
+target.thumb_img = ''
+
+target.pieces = obj.children.find(v => {
+	return v.type === 'group' && v.name === 'block'
+}).children.map(({bottom, left, top, right}) => {
+	return {
+		left,
+		top,
+		right,
+		bottom
+	}
+})
+
+target.pic_count = target.pieces.length
+
+fs.writeFileSync('./test.json', JSON.stringify(target, undefined, 4))
+// psd.image.saveAsPng('./s.png').then(() => {})
+// let tree = psd.tree()
+
+
+// console.log(tree.export())
